314bc7116d42bd79714d1b262a485e935ed04350
its-bismay <2023ugee070@nitjsr.ac.in>
2025-12-22 22:41:42 +0530

    inngest integration test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { pdfProcessRouter } from "./routes/pdfProcessRoute.js";
+import { functions, inngest } from "./inngest/inngestClient.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json())
 app.get("/", (_req, res) => {
     res.status(200).send("server is up and running")
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api", pdfProcessRouter)
 
