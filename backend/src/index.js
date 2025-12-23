c692ca891ff9fd1987c2586e8890330e18fa1ec8
its-bismay <2023ugee070@nitjsr.ac.in>
2025-12-23 13:22:28 +0530

    html page added to the home route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
-import express from "express"
+import express from "express";
 import { serve } from "inngest/express";
 import { pdfProcessRouter } from "./routes/pdfProcessRoute.js";
 import { functions, inngest } from "./inngest/inngestClient.js";
 import { queryRouter } from "./routes/queryRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -10,7 +16,7 @@ app.use(express.json())
 
 
 app.get("/", (_req, res) => {
-    res.status(200).send("server is up and running")
+    res.sendFile(path.join(__dirname, "../public/page.html"));
 })
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
